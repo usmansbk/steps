984e740209bd5b591225949e8e2c502ca803e36e
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-08 09:56:14 +0100

    add dependency

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Provider as PaperProvider} from 'react-native-paper';
+import {NavigationContainer} from '@react-navigation/native';
 import App from './src/App';
 
 export default () => {
   return (
     <PaperProvider>
-      <App />
+      <NavigationContainer>
+        <App />
+      </NavigationContainer>
     </PaperProvider>
   );
 };
