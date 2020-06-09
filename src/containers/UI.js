0929dc6df4356cd8a1e081f76be58b210dcb144b
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-09 11:19:49 +0100

    chore

--- src/containers/UI.js
+++ src/containers/UI.js
@@ -3,7 +3,7 @@ import {Container} from 'unstated';
 export default class UIContainer extends Container {
   state = {
     firstLaunch: true,
-    userName: 'Usman',
+    userName: 'Friend',
   };
 
   setName = (userName) => {
