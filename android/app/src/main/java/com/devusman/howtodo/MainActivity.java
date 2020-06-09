f0f32fdc2fabef50bb0e12a8150009763cb6c57f
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-10 00:31:19 +0100

    rename package

--- android/app/src/main/java/com/devusman/howtodo/MainActivity.java
+++ android/app/src/main/java/com/devusman/howtodo/MainActivity.java
@@ -1,4 +1,4 @@
-package com.steps;
+package com.devusman.howtodo;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "steps";
+    return "HowToDo";
   }
 }
--- ios/HowToDoTests/HowToDoTests.m
+++ ios/HowToDoTests/HowToDoTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface stepsTests : XCTestCase
+@interface HowToDoTests : XCTestCase
 
 @end
 
-@implementation stepsTests
+@implementation HowToDoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
