590e805aa7e87f8e90295b0684b47656ec336c12
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-09 09:24:13 +0100

    chore

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
  * @format
  */
 import 'react-native-gesture-handler';
-import 'mobx-react-lite/batchingForReactNative';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
