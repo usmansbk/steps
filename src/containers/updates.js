f0953ca9a0b030203219e03179e541d872640af0
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-12 18:42:14 +0100

    chore

---
+++ src/containers/updates.js
@@ -0,0 +1,27 @@
+export default {
+  id: '__update__june__12__',
+  title: "We've made it even better",
+  category: '#ChangeLog',
+  date: new Date().toISOString(),
+  steps: [
+    {
+      id: 1,
+      key: 1,
+      label:
+        'You can now grab recipes from popular sites. Just paste your link and press the enter key',
+      photo: require('../assets/entertitle.jpg'),
+    },
+    {
+      id: 2,
+      key: 2,
+      label: 'You can now Edit a step without deleting it',
+      photo: require('../assets/entertitle.jpg'),
+    },
+    {
+      id: 3,
+      key: 3,
+      label: 'We now now have a separate field for ingredients',
+      photo: require('../assets/entertitle.jpg'),
+    },
+  ],
+};
