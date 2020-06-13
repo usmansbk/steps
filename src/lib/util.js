168ce75bf92376534ecb33960c19cb2d586d6f72
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-13 22:38:37 +0100

    chore

--- src/lib/util.js
+++ src/lib/util.js
@@ -3,21 +3,10 @@ import ImagePicker from 'react-native-image-picker';
 import {VALUE} from './constants';
 
 export function filterByQuery(data, query) {
-  const searchRegex = /(#\w+\S)?\s?([\w\s\W]+)/gi;
-
-  const matches = searchRegex.exec(query);
   return data.filter((item) => {
-    let matchCategory = true;
-    let matchTitle = true;
-    if (matches) {
-      const [, category, title] = matches;
-      matchTitle = item.title.toLocaleLowerCase().includes(title);
-      if (category) {
-        let cat = category.slice(1);
-        matchCategory = item.category.toLocaleLowerCase().includes(cat);
-      }
-    }
-    return matchCategory && matchTitle;
+    const title = item.title.toLocaleLowerCase();
+    const category = item.category.toLocaleLowerCase();
+    return title.includes(query) || category.includes(query);
   });
 }
 
